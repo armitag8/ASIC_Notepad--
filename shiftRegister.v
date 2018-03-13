d6c011bc53d5bab2566367ab9359c871e863faf2
armitag8 <joe.armitage@mail.utoronto.ca>
2018-03-13 14:59:32 -0400

    Various bugfixes and signature refactorization applied to shiftRegister.v

--- shiftRegister.v
+++ shiftRegister.v
@@ -1,23 +1,25 @@
-module shiftRegister(value, shift, assign, reset, clock, result)
-    input [127:0] value;
-    input shift;
-    input clock;
-    input reset;
-    input assign;
-    output result;
+module shiftRegister
+	(
+		output result,
+		input [127:0] load_val,
+		input load_n, 
+		input shift, 
+		input reset, 
+		input clock
+	);
     
     reg pixel[127:0];
     
-    assign result = output[127];
+    assign result = pixel[127];
     
     always @(posedge clock)
 	begin 
         if(reset == 1'b0)
             pixel <= 128'd0;
-        else if (assign == 1'b1)
-            pixel <= value;
+        else if (load_n == 1'b1)
+            pixel <= load_val;
         else if (shift == 1'b1)
-            pixel << 1'b0;
+            pixel << 1'b1;
     end
 
 endmodule
