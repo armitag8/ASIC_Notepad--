0a8dc9f231254f09e9422ddcdd9408dbb4c245ba
Joe Armitage <joe.armitage@mail.utoronto.ca>
2018-03-27 12:12:00 -0400

    Added rate divider; moved pin assignment file to root directory.

---
+++ src/com/rate_divider.v
@@ -0,0 +1,21 @@
+module rate_divider(
+        output reg OUT,
+        input IN
+    );
+    
+    reg [27:0] COUNTER, MAX;
+    localparam MAX <= 28'd100_000_000;
+    
+    always @(posedge IN)
+    begin
+        if (COUNTER == 0) 
+            OUT <= 1'b0;
+        COUNTER <= COUNTER + 1'b1;
+        if (COUNTER == MAX)
+        begin
+            OUT <= 1'b1;
+            COUNTER <= 0;
+        end            
+    end
+
+endmodule
