71af74a0b899e4c9919dcfad92c0a88e5e325fef
Robert Augustynowicz <augusty6@IITS-I402-04.utsc-labs.utoronto.ca>
2018-03-27 19:08:15 -0400

    Fixed use of bitwise operators.

--- src/com/counter_4bit.v
+++ src/com/counter_4bit.v
@@ -15,15 +15,15 @@ module counter_4bit
     
     always @(posedge CLK)
     begin
-        if (increase == 1'b1 | decrease == 1'b1 | CLR == 1'b1 | parallel == 1'b1)
+        if (increase == 1'b1 || decrease == 1'b1 || CLR == 1'b1 || parallel == 1'b1)
         begin
             if (CLR == 1'b1)
                 Q_OUT[3:0] <= 4'b0000;
-            else if (CLK == 1'b1 & increase == 1'b1)
+            else if (CLK == 1'b1 && increase == 1'b1)
                Q_OUT[3:0] <= Q_OUT[3:0] + 4'b0001;
-            else if (CLK == 1'b1 & decrease == 1'b1)
+            else if (CLK == 1'b1 && decrease == 1'b1)
                Q_OUT[3:0] <= Q_OUT[3:0] - 4'b0001;
-            else if (CLK == 1'b1 & parallel == 1'b1)
+            else if (CLK == 1'b1 && parallel == 1'b1)
                Q_OUT[3:0] <= load[3:0];
         end
     end
