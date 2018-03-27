1c80fcac8f303b28eb86216e59cb3228d37d47a3
Joe Armitage <joe.armitage@mail.utoronto.ca>
2018-03-27 15:09:41 -0400

    Added parallel load and decrement.

--- src/com/counter_4bit.v
+++ src/com/counter_4bit.v
@@ -1,7 +1,10 @@
 module counter_4bit
     (
-        output reg [3:0] Q_OUT, 
-        input EN, 
+        output reg [3:0] Q_OUT,
+        input [3:0] load,
+        input parallel,
+        input increase, 
+        input decrease, 
         input CLK, 
         input CLR
     );
@@ -12,12 +15,16 @@ module counter_4bit
     
     always @(posedge CLK)
     begin
-        if (EN == 1'b1 | CLR == 1'b1)
+        if (increase == 1'b1 | decrease == 1'b1 | CLR == 1'b1 | parallel == 1'b1)
         begin
             if (CLR == 1'b1)
                 Q_OUT[3:0] <= 4'b0000;
-            else if (CLK == 1'b1)
+            else if (CLK == 1'b1 & increase == 1'b1)
                Q_OUT[3:0] <= Q_OUT[3:0] + 4'b0001;
+            else if (CLK == 1'b1 & decrease == 1'b1)
+               Q_OUT[3:0] <= Q_OUT[3:0] - 4'b0001;
+            else if (CLK == 1'b1 & parallel == 1'b1)
+               Q_OUT[3:0] <= load[3:0];
         end
     end
 endmodule
--- src/com/counter_6bit.v
+++ src/com/counter_6bit.v
@@ -1,7 +1,10 @@
 module counter_6bit
     (
-        output reg [5:0] Q_OUT, 
-        input EN, 
+        output reg [5:0] Q_OUT,
+        input [5:0] load,
+        input parallel,
+        input increase, 
+        input decrease, 
         input CLK, 
         input CLR
     );
@@ -12,12 +15,16 @@ module counter_6bit
 
     always @(posedge CLK)
     begin
-        if (EN == 1'b1 | CLR == 1'b1)
+        if (increase == 1'b1 | decrease == 1'b1 | CLR == 1'b1 | parallel == 1'b1)
         begin
             if (CLR == 1'b1)
                 Q_OUT[5:0] <= 6'b00_0000;
-            else if (CLK == 1'b1)
+            else if (CLK == 1'b1 & increase == 1'b1)
                Q_OUT[5:0] <= Q_OUT[5:0] + 6'b00_0001;
+            else if (CLK == 1'b1 & decrease == 1'b1)
+               Q_OUT[5:0] <= Q_OUT[5:0] - 6'b00_0001;
+            else if (CLK == 1'b1 & parallel == 1'b1)
+               Q_OUT[3:0] <= load[5:0];
         end
     end
 
